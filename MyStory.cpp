883c73610a1ac49865082970bc6a9f05fe388cbf
Eric Sigg <esigg@browning.edu>
2017-07-03 15:38:04 -0400

    commit3

---
+++ MyStory.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string robotName, robotFeeling;
+	cout << "What is the robot's name?"<< endl;
+	cin >> robotName;
+	cout << "How is the robot feeling?" << endl;
+	cin >> robotFeeling;
+	cout << "Once upon a time, there was a robot named " << robotName << endl;
+    cout << robotName << " was very "<< robotFeeling << endl;
+    cout << robotName << " was built by his friend Don." << endl;
+    cout << "One day, " << robotName << " decided to go on a scavenger hunt." << endl;
+    cout << "On the scavenger hunt, " << robotName << " didn't see the edge of the cliff and fell off."<< endl;
+    cout << robotName << " bent his wheel and now, he cannot move straight anymore." << endl;
+    cout << "To this day, " << robotName << " cannot seem to avoid walls." << endl;
+}
