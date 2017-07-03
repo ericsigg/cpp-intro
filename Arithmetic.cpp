7b05573fc9110bd1d9cac62f376a4abb63bcb0b2
Eric Sigg <esigg@browning.edu>
2017-07-03 15:56:19 -0400

    commit3

---
+++ Arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int one = 1;
+	int two = 2;
+	
+	 one = one + 10;
+	cout << one <<endl;
+	cout << two << endl;
+	one *= two;
+	cout << one << endl;
+	one++;
+	cout << one << endl;
+}
