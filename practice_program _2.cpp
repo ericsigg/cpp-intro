3b2fac9610687f69642c515675dd85ee3499ebaa
Eric Sigg <esigg@browning.edu>
2017-07-03 15:20:52 -0400

    commit2

---
+++ practice_program _2.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+	string myName;
+	cout << "Enter your name" << endl;
+	cin >> myName;
+	cout << "Hello " << myName <<endl;
+}
