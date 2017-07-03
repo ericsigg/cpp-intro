f571277a8e9b76de007cc97cbd55958cffe84646
Eric Sigg <esigg@browning.edu>
2017-07-03 15:10:02 -0400

    commit1

---
+++ practice_program_1.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	bool iAmRealCamper = true;
+	double fourPointFour = 4.4;
+	int myHairCount = 2;
+	char questionMark = '?';
+	string myName = "Eric";
+    string advice = "Use a toaster shield, fight!";
+	
+	cout << fourPointFour << "and" << fourPointFour << endl;	
+
+    cout << "Welcome to my first program!" << endl;
+    cout << "My name is" << myName << endl;
+    cout << "I am not bald, I have" << myHairCount << "hairs" << endl;
+    cout << "My advice is:" << advice << questionMark << endl;
+    cout << "I am a real camper. This is " << iAmRealCamper << endl;
+}
